9534b54c646c206f1bf9cfec90f55ad23c4fc1d4
NickR23 <nic.rr@icloud.com>
2025-05-22 14:58:04 -0400

    Remove main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "hash/sha1.h"
-
-int main(void) {
-    std::string message = "Hash me please!";
-    std::string result = hash::sha1::hash(message);
-    std::cout << result << std::endl;
-    return 0;
-}
