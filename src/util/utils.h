1d15a2b87709378f890fff8683e8265ad1686a96
NickR23 <nic.rr@icloud.com>
2025-05-22 09:43:43 -0400

    Add support for size(buf) > 512 in sha1_pad

--- src/util/utils.h
+++ src/util/utils.h
@@ -3,7 +3,10 @@
 using Buffer = std::vector<uint8_t>;
 
 namespace utils {
-    #define WORD_LEN 32
+    // Lengths in bits
+    constexpr int WORD_LEN = 32;
+    constexpr int BLOCK_LEN = 512;
+    constexpr int BYTE_LEN = 8;
 
     inline Buffer to_buffer(const std::string& s) {
         return Buffer(s.begin(), s.end());
@@ -19,22 +22,21 @@ namespace utils {
      * SHA-1 as `n` 512-bit blocks.
     */
     void sha1_pad(Buffer& buf) {
-        uint64_t len = buf.size() * 8; // Num bits in original message
+        uint64_t len = buf.size() * BYTE_LEN; // Num bits in original message
         // Append 1 (0x80)
         buf.push_back(0x80);
 
         // Pad with 0s
-        // TODO add support for messsages > 512 bits.
-        // This should append until you are 64 bits short of a multiple of 512.
-        auto target = 512 - (2 * WORD_LEN);
-        while (buf.size() % (target / 8) != 0) {
+        // This should append until you are 64 bits (2 words) short of a multiple of 512.
+        // This extra space is for the 2 word sized length the be appended.
+        auto length_pad = (2 * WORD_LEN) / BYTE_LEN;
+        while ((buf.size() + length_pad) % (BLOCK_LEN / BYTE_LEN) != 0) {
             buf.push_back(0x00);
         }
 
         // Append buf length to padded buf. 
-        // This was found using my own blood sweat and tears.
-        for (int i = 0; i < 8; ++i) {
-            int shift = (7 - i) * 8;
+        for (int i = 0; i < BYTE_LEN; ++i) {
+            int shift = ((BYTE_LEN - 1) - i) * BYTE_LEN;
 
             // Grab a byte by shifting over i times and masking.
             uint8_t byte = static_cast<uint8_t>((len >> shift) & 0xFF);
--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -11,7 +11,7 @@ TEST_P(UtilsPaddingTest, SHA1PadTest) {
     Buffer buf = utils::to_buffer(message);
     utils::sha1_pad(buf);
 
-    EXPECT_EQ(buf.size() % (512 / 8), 0); // length should be blocks of 512 bits
+    EXPECT_EQ(buf.size() % (utils::BLOCK_LEN / utils::BYTE_LEN), 0); // length should be blocks of 512 bits
 
     // Check message
     for (int i = 0; i < message.size(); i++) {
@@ -19,17 +19,19 @@ TEST_P(UtilsPaddingTest, SHA1PadTest) {
     }
 
     // Zero pad
-    for (int i = message.size() + 1; i < buf.size() - (64 / 8); i++) {
+    uint zero_pad_range = buf.size() - ((utils::WORD_LEN  * 2) / utils::BYTE_LEN);
+    for (int i = message.size() + 1; i < zero_pad_range; i++) {
         EXPECT_EQ(buf[i], 0x00);
     }
 
     // Check encoded length
     uint64_t len = 0;
-    for (int i = buf.size() - 4; i < buf.size(); i++) {
-        len <<= 8;
+    const uint encoded_len_length = utils::WORD_LEN / utils::BYTE_LEN;
+    for (int i = buf.size() - encoded_len_length; i < buf.size(); i++) {
+        len <<= utils::BYTE_LEN;
         len |= buf[i];
     }
-    EXPECT_EQ(len, message.size() * 8);
+    EXPECT_EQ(len, message.size() * utils::BYTE_LEN);
 }
 
 INSTANTIATE_TEST_SUITE_P(
