a98918104546c5cfe4ce1630a3690f1593935574
NickR23 <nic.rr@icloud.com>
2025-05-22 11:38:04 -0400

    Add buffer chunking step

--- src/hash/sha1.h
+++ src/hash/sha1.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <optional>
 
 #include <util/utils.h>
 
@@ -6,10 +7,31 @@ namespace hash::sha1 {
     constexpr int SHA1_WORD_LEN = 32;
     constexpr int SHA1_BLOCK_LEN = 512;
 
-    std::string hash(const std::string& message) {
-        Buffer buf = utils::to_buffer(message);
-        return "Not implemented...";
-    }
+    // These constants are part of the standard
+    // https://datatracker.ietf.org/doc/html/rfc3174#section-5
+    constexpr int K_1 = 0x5A827999;
+    constexpr int K_2 = 0x6ED9EBA1;
+    constexpr int K_3 = 0x8F1BBCDC;
+    constexpr int K_4 = 0xCA62C1D6;
+
+    struct Sha1_context {
+        /**
+         *    The message digest is computed using the message padded as described
+         *    in section 4.  The computation is described using two buffers, each
+         *    consisting of five 32-bit words, and a sequence of eighty 32-bit
+         *    words.  The words of the first 5-word buffer are labeled A,B,C,D,E.
+         *    The words of the second 5-word buffer are labeled H0, H1, H2, H3, H4.
+         *    The words of the 80-word sequence are labeled W(0), W(1),..., W(79).
+         *    A single word buffer TEMP is also employed.
+         */
+        std::array<uint32_t, 5> b1 = {0x0,0x0,0x0,0x0,0x0};
+        std::array<uint32_t, 5> b2 = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
+        std::array<uint32_t, 80> w;
+        std::vector<uint32_t> temp;
+
+        std::vector<uint32_t> m; // Message digest buffer. Split into 16 word (512 bits) blocks
+    };
+
 
     /** 
      * The point of this padding is to make the length a multiple of 512.
@@ -43,4 +65,44 @@ namespace hash::sha1 {
             buf.push_back(byte);
         }
     }
+
+    std::optional<std::vector<uint32_t>> toMessageDigestBuffer(const Buffer& padded_message) {
+        std::vector<uint32_t> m_digest_buffer;
+        size_t num_words = padded_message.size() / 4;
+        m_digest_buffer.reserve(num_words);
+
+        for (size_t i = 0; i < num_words; ++i) {
+            uint32_t w = 0;
+            for (size_t j = 0; j < 4; ++j) {
+                size_t byte_index = i * 4 + j; // Index should be shifted a word (4 bytes) and 'j' over.
+                if (byte_index < padded_message.size()) {
+                    w |= (static_cast<uint32_t>(padded_message[byte_index]) << ((3 - j) * 8));
+                } else {
+                    return std::nullopt;
+                }
+            }
+            m_digest_buffer.push_back(w);
+        }
+        return m_digest_buffer;
+    }
+
+    std::optional<Sha1_context> makeContext(const std::string& message) {
+        Sha1_context ctx;
+        Buffer buf = utils::to_buffer(message);
+        sha1_pad(buf);
+        auto digest_buffer = toMessageDigestBuffer(buf);
+        if (!digest_buffer) {
+            return std::nullopt;
+        }
+        ctx.m = digest_buffer.value();
+        return ctx;
+    }
+
+    std::string hash(const std::string& message) {
+        Buffer buf = utils::to_buffer(message);
+        return "Not implemented...";
+    }
+
+
+
 }
--- test/sha1_test.cpp
+++ test/sha1_test.cpp
@@ -4,10 +4,22 @@
 #include <test/assets/words.h>
 
 using namespace hash;
-class SHA1PaddingTest
+class SHA1Tests
   : public ::testing::TestWithParam<std::string> {};
 
-TEST_P(SHA1PaddingTest, SHA1PadTest) {
+TEST(SHA1Tests, MakeContextTest) {
+    const std::string& message = "I need context!";
+    auto gotContext = sha1::makeContext(message);
+    ASSERT_TRUE(gotContext);
+    EXPECT_EQ(gotContext->b1.size(), 5);
+    for (auto elem : gotContext->b1) {
+        EXPECT_EQ(elem, 0x0);
+    }
+    EXPECT_EQ(gotContext->b2.size(), 5);
+    EXPECT_EQ(gotContext->w.size(), 80);
+}
+
+TEST_P(SHA1Tests, SHA1PadTest) {
     const std::string& message = GetParam();
     Buffer buf = utils::to_buffer(message);
     sha1::sha1_pad(buf);
@@ -35,7 +47,28 @@ TEST_P(SHA1PaddingTest, SHA1PadTest) {
     EXPECT_EQ(len, message.size() * utils::BYTE_LEN);
 }
 
+TEST_P(SHA1Tests, ToMessageDigestBufferTest) {
+    const std::string& message = GetParam();
+    Buffer buf = utils::to_buffer(message);
+    sha1::sha1_pad(buf); 
+    auto got_buffer = sha1::toMessageDigestBuffer(buf);
+    ASSERT_TRUE(got_buffer.has_value());
+
+    // Each word should be 4 bytes
+    ASSERT_EQ(buf.size() % 4, 0);
+    ASSERT_EQ(got_buffer->size(), buf.size() / 4);
+
+    // Check the words
+    for (size_t i = 0; i < got_buffer->size(); i++) {
+        uint32_t expectedWord = 0;
+        for (size_t j = 0; j < 4; ++j) {
+            expectedWord |= (static_cast<uint32_t>(buf[i * 4 + j]) << ((3 - j) * 8));
+        }
+        EXPECT_EQ(got_buffer.value()[i], expectedWord);
+    }
+}
+
 INSTANTIATE_TEST_SUITE_P(
-    SHA1PaddingTests,  SHA1PaddingTest,
+    SHA1TestVector,  SHA1Tests,
     ::testing::ValuesIn(testing::test_vector)
 );
